2092b20f4d660bcaa57c7ccb77f4c417026a326a
OlegKrikun <oleg.krikun@gmail.com>
2012-05-17 13:09:16 +0400

    more improvements for loader

--- src/ru/krikun/s2e/Loader.java
+++ src/ru/krikun/s2e/Loader.java
@@ -26,37 +26,22 @@ import java.io.InputStream;
 import java.util.List;
 
 class Loader extends AsyncTask<Void, Void, Void> {
+    //Path to userinit dir
+    private static final String PATH_USERINIT = "/data/local/userinit.d";
     //Path to bin dir
-    private static final String PATH_BIN =
-            "/data/local/bin";
-    //Path to installed script
-    private static final String PATH_SCRIPT =
-            "/data/local/userinit.d/simple2ext";
-    //Path to tune2fs and e2fsck
-    private static final String PATH_TUNE2FS =
-            PATH_BIN + "/tune2fs";
-    private static final String PATH_E2FSCK =
-            PATH_BIN + "/e2fsck";
-    //Shell command for make userinit dir
-    private static final String SHELL_MAKE_USERINIT_DIR =
-            "if [ ! -e /data/local/userinit.d ]; then busybox install -m 777 -o 1000 -g 1000 -d /data/local/userinit.d; fi";
-    //Shell command for make bin dir
-    private static final String SHELL_MAKE_BIN_DIR =
-            "busybox install -m 777 -o 1000 -g 1000 -d /data/local/bin";
-    //Shell command for copy script from app home to userinit.d
-    private static final String SHELL_COPY_SCRIPT_TO_USERINIT_DIR =
-            "busybox cp " + Helper.S2E_DIR + "/files/script01.sh " + PATH_SCRIPT;
-    //Shell commands for copy tune2fs and e2fsck from app home to bin
-    private static final String SHELL_COPY_TUNE2FS_TO_BIN_DIR =
-            "busybox cp " + Helper.S2E_DIR + "/files/tune2fs " + PATH_TUNE2FS;
-    private static final String SHELL_COPY_E2FSCK_TO_BIN_DIR =
-            "busybox cp " + Helper.S2E_DIR + "/files/e2fsck " + PATH_E2FSCK;
-    //Shell command for set permission to script
-    private static final String SHELL_SET_PERMISSION =
-            "busybox chmod 0777 ";
+    private static final String PATH_BIN = "/data/local/bin";
+    //Files names of script, tune2fs and e2fsck
+    private static final String SCRIPT = "simple2ext";
+    private static final String TUNE2FS = "tune2fs";
+    private static final String E2FSCK = "e2fsck";
+    //Shell command templates for make dir
+    private static final String SHELL_MAKE_DIR = "busybox install -m 777 -o 1000 -g 1000 -d ";
+    //Shell commands templates for copy tools from app home to bin
+    private static final String SHELL_COPY_FILE = "busybox cp ";
+    //Shell command templates for set permission to script
+    private static final String SHELL_SET_PERMISSION = "busybox chmod 0777 ";
     //Shell command for get md5 sum
-    private static final String SHELL_GET_MD5 =
-            "busybox md5sum " + PATH_SCRIPT;
+    private static final String SHELL_GET_MD5 = "busybox md5sum " + PATH_USERINIT + "/" + SCRIPT;
 
     private final App app;
     private ProgressDialog dialog;
@@ -66,12 +51,11 @@ class Loader extends AsyncTask<Void, Void, Void> {
 
     public Loader(Main main) {
         this.main = main;
-
         app = App.getInstance();
 
         //Getting md5 from resources
         md5 = App.getRes().getString(R.string.script_md5);
-
+        //Create ProgressDialog
         dialog = new ProgressDialog(main);
     }
 
@@ -80,63 +64,37 @@ class Loader extends AsyncTask<Void, Void, Void> {
 
         //Check API
         app.loadAPI();
-
-        //Stop if API not supported
         if (app.isSupportedOS()) {
 
             //Check shell
             app.loadShell();
-
-            //Stop if root, busybox not available or access not given
             if (app.isRoot()) {
 
                 //Check script
                 if (!checkScript()) {
-
-                    //Installing script
-                    copyFileToHome("script01.sh");
-                    //Final actions of script installation
+                    //Installing script to home dir
+                    copyFileToHome(SCRIPT);
                     //Check /data/local/userinit.d exists and create this if needed
+                    makeDirShell(PATH_USERINIT);
                     //Copy script and set permission
-                    Helper.sendShell(SHELL_MAKE_USERINIT_DIR);
-                    Helper.sendShell(SHELL_COPY_SCRIPT_TO_USERINIT_DIR);
-                    Helper.sendShell(SHELL_SET_PERMISSION  + PATH_SCRIPT);
-
-                    //Check script, again
-                    if (checkScript()) {
-
-                        //Set "Script Installed"
+                    copyFileShell(SCRIPT, PATH_USERINIT);
+                    //Check script again and set "Script Installed" if script exists
+                    if (checkScript())
                         app.setScriptInstalled(true);
-                    }
                 } else {
-
                     //Set "Script Installed"
                     app.setScriptInstalled(true);
                 }
 
                 //Check /data/local/bin exists and create this if needed
-                if(!Helper.checkFileExists(PATH_BIN))
-                    Helper.sendShell(SHELL_MAKE_BIN_DIR);
-
-                if (!Helper.checkFileExists(PATH_TUNE2FS)) {
-                    //Install tune2fs
-                    copyFileToHome("tune2fs");
-                    //Copy tune2fs and set permission
-                    Helper.sendShell(SHELL_COPY_TUNE2FS_TO_BIN_DIR);
-                    Helper.sendShell(SHELL_SET_PERMISSION  + PATH_TUNE2FS);
-                }
-
-                if (!Helper.checkFileExists(PATH_E2FSCK)) {
-                    //Install e2fsck
-                    copyFileToHome("e2fsck");
-                    //Copy e2fsck and set permission
-                    Helper.sendShell(SHELL_COPY_E2FSCK_TO_BIN_DIR);
-                    Helper.sendShell(SHELL_SET_PERMISSION  + PATH_E2FSCK);
-                }
+                makeDirShell(PATH_BIN);
+                //Install tune2fs to home, copy to bin and set permission
+                installTool(TUNE2FS);
+                //Install e2fsck to home, copy to bin and set permission
+                installTool(E2FSCK);
 
                 //Load targets set (sizes, statuses and etc)
                 app.loadTargets();
-
                 //Load partitions set
                 app.loadPartitions();
             }
@@ -165,37 +123,19 @@ class Loader extends AsyncTask<Void, Void, Void> {
                 Log.e(Helper.TAG, "Error in dialog.dismiss()");
             }
         }
-//        //Exit if OS not supported
-//        if (!app.isSupportedOS()) main.showAlert(App.getRes().getString(R.string.alert_version));
-//        //Exit if access to root not given or busybox not found
-//        else if (!app.isRoot()) main.showAlert(App.getRes().getString(R.string.alert_root));
-//        //Exit if script not installed
-//        else if (!app.isScriptInstalled()) main.showAlert(App.getRes().getString(R.string.alert_script_title));
-//        //Load main view
-//        else main.onTaskFinished();
+        //Exit if OS not supported
+        if (!app.isSupportedOS()) main.showAlert(App.getRes().getString(R.string.alert_version));
+        //Exit if access to root not given or busybox not found
+        else if (!app.isRoot()) main.showAlert(App.getRes().getString(R.string.alert_root));
+        //Exit if script not installed
+        else if (!app.isScriptInstalled()) main.showAlert(App.getRes().getString(R.string.alert_script_title));
+        //Load main view
+        else main.onTaskFinished();
     }
 
     //Check script exists and control md5
     private boolean checkScript() {
-        return checkScriptExists() && checkScriptSum();
-    }
-
-//    //Final actions of script installation
-//    //Check /data/local/userinit.d exists and create this if needed
-//    //Copy script and set permission
-//    private void copyScriptToUserInit() {
-//        Helper.sendShell(SHELL_MAKE_USERINIT_DIR);
-//        Helper.sendShell(SHELL_COPY_SCRIPT_TO_USERINIT_DIR);
-//        Helper.sendShell(SHELL_SET_PERMISSION  + PATH_SCRIPT);
-//    }
-
-    //Final actions of tune2fs installation
-    //Check /data/local/bin exists and create this if needed
-    //Copy tune2fs and set permission
-    private void copyTune2FsToBin() {
-        Helper.sendShell(SHELL_MAKE_BIN_DIR);
-        Helper.sendShell(SHELL_COPY_TUNE2FS_TO_BIN_DIR);
-        Helper.sendShell(SHELL_SET_PERMISSION  + PATH_TUNE2FS);
+        return Helper.checkFileExists(PATH_USERINIT + "/" + SCRIPT) && checkScriptSum();
     }
 
     //Return md5sum of path
@@ -207,15 +147,32 @@ class Loader extends AsyncTask<Void, Void, Void> {
         return null;
     }
 
-    //Check script exists
-    private boolean checkScriptExists() {
-        return Helper.checkFileExists(PATH_SCRIPT);
-    }
-
     //Compare md5sum
     private boolean checkScriptSum() {
         String tmp_md5 = getSum();
         return tmp_md5 != null && tmp_md5.equals(md5);
+    }
+
+    //Install tool to bin dir
+    private void installTool(String toolName) {
+        if (!Helper.checkFileExists(PATH_BIN + "/" + toolName)) {
+            //Install tool to home
+            copyFileToHome(toolName);
+            //Copy tool to bin and set permission
+            copyFileShell(toolName, PATH_BIN);
+        }
+    }
+
+    //Copy tools to bin and set permission
+    private void copyFileShell(String fileName, String destinationDir) {
+        Helper.sendShell(SHELL_COPY_FILE + Helper.S2E_DIR + "/files/" + fileName + " " + destinationDir + "/" + fileName);
+        Helper.sendShell(SHELL_SET_PERMISSION  + destinationDir + "/" + fileName);
+    }
+
+    //Make dir via shell
+    private void makeDirShell(String dirPath) {
+        if(!Helper.checkFileExists(dirPath))
+            Helper.sendShell(SHELL_MAKE_DIR + dirPath);
     }
 
     //Extract file and copy to app home dir
